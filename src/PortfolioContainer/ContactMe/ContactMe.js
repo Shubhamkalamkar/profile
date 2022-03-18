bd473e981809cb19b55ffab25fc145e95dc71e4d
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2022-03-18 10:08:35 +0530

    email integration

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import './ContactMe.css'
+import emailjs from 'emailjs-com'
 
 export default function ContactMe() {
+    
+    function sendEmail(e){
+        e.preventDefault();
+
+        emailjs.sendForm('service_5mue3ij', 'template_3lupsql', e.target,"1KaRrTPk_CU6A7_O7").then(res=>{
+            console.log(res);
+        }).catch(err=> console.log(err));
+    }
   return (
       <div className='container'>
-          <form>
+          <form onSubmit={sendEmail}>
               <h3>ContactMe</h3>
-              <input type="text" id='name' placeholder='Enter Your Name' required />
-              <input type="email" id='email' placeholder='Enter Your Email Id' required />
-              <input type="text" id='phone' placeholder='Enter Your Phone Number' required />
-              <textarea id='message' rows="5" placeholder='How Can We Help You?'></textarea>
+              <input type="text" name='name' placeholder='Enter Your Name' required />
+              <input type="email" name='email' placeholder='Enter Your Email Id' required />
+              <input type="text" name='phone' placeholder='Enter Your Phone Number' required />
+              <textarea name='message' rows="5" placeholder='How Can We Help You?'></textarea>
               <button type='submit'>Send Message</button>
           </form>
           
